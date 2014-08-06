63685c02ce1f288ee569b2702f6ab9f20021a961
Markus Kefka's <markusdarkness@gmail.com>
2014-08-06 02:00:39 -0300

    added cli tool

---
+++ zuera.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env
+
+var program = require('commander');
+
+program
+  .version('0.0.3')
+  .option('start', 'Start the zuera')
+  .option('quote', 'A cool quote about the zuera')
+  .option('end', 'Stop the zuera')
+  .parse(process.argv);
+
+if(program.start){
+    while(true)
+        console.log('The Zuera has been planted');
+}
+
+if(program.end){
+    while(true)
+        console.log('You can stop the Zuera, because the Zuera never ends');
+}
+
+if(program.quote){
+    var quotes = require('./res/quotes.json');
+
+    var random = Math.floor(Math.random() * quotes.length);
+    var randomQuote = quotes[random];
+
+    console.log(randomQuote);
+}
